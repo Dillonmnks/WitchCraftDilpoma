923496fb69422d6f0d589e39677bd7515e945bf2
Dillon <dillon.monks2104@gmail.com>
2025-02-13 19:48:34 +0100

    vs bs

--- Source/HexenHaus/HexenHaus.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "HexenHaus.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HexenHaus, "HexenHaus" );
